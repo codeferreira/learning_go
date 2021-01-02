04d6066be89cf42574d34b0d3097d122bbba1f2b
José Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-01-01 23:00:00 -0300

    chore: create a new simple function to teste compiler

--- first_steps/hello.go
+++ first_steps/hello.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// Global package variable declaration
+// Package scope variable declaration
 var y = "Hello World!"
 
 func main() {
@@ -13,4 +13,13 @@ func main() {
 
 	// Print simple line on prompt
 	fmt.Println(x)
+}
+
+// Creating a new function on package
+func printLine(x string) {
+	// Print receaved variable on function paramaters
+	fmt.Println(x)
+
+	// Print package scope variable
+	fmt.Println(y)
 }
