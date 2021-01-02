629df8627e20fe68736ff0fb09fd3d3e6524096e
José Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-01-01 22:52:15 -0300

    chore: add comments to understand basic concepts

---
+++ first_steps/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Global package variable declaration
+var y = "Hello World!"
+
+func main() {
+	// Auto type variable creation
+	x := 10
+
+	// Print simple line on prompt
+	fmt.Println(x)
+}
